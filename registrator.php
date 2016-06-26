d9258ad4cce6e741f0b5fd82684e5769b9cb06e1
ents <entsupml@gmail.com>
2016-06-26 21:27:17 +0300

    fix

--- registrator.php
+++ registrator.php
@@ -39,7 +39,7 @@ for (;;) {
 				if (!empty($result['errors']) && empty($result['errors']['captcha'])) {
 					message("Captcha recognized successful, but other error found");
 					$json = json_encode($result, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
-					if (mb_strlen($json, 'utf8') < 200) {
+					if (mb_strlen($json, 'utf8') < 100) {
 						$mess = "Response: $json";
 					} else {
 						$mess = "See details at log";
